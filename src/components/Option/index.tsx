fa799dce336a318e3a0c3f85179dde001ac6c2ad
William Grohe <william.grohe@gmail.com>
2022-05-08 19:37:06 -0300

    Created feedback buttons/options of modal

---
+++ src/components/Option/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  View,
+  TouchableOpacity,
+  TouchableHighlightProps,
+  Image,
+  ImageProps,
+  Text
+} from "react-native";
+
+import { styles } from "./styles";
+
+interface Props extends TouchableHighlightProps {
+  title: string;
+  image: ImageProps;
+}
+
+export function Option({title, image, ...rest}: Props) {
+  return (
+  <TouchableOpacity 
+    style={styles.container}
+    {...rest}
+  >
+    <Image 
+    source={image}
+    style={styles.image}
+    />
+
+    <Text style={styles.title}>
+      {title}
+    </Text>
+  </TouchableOpacity>
+  )
+}
